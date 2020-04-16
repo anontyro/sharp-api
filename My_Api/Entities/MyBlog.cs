534d68ebae9118053a3cca545945965aef92962a
alexWilkinson <alex@hooq.tv>
2020-04-16 15:39:39 +0800

    database entities

---
+++ My_Api/Entities/MyBlog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace My_Api
+{
+    public partial class MyBlog
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string Tags { get; set; }
+        public int Author { get; set; }
+        public string CoverImage { get; set; }
+        public string ThumbNail { get; set; }
+        public string PermaLink { get; set; }
+        public int? TimeRequired { get; set; }
+        public DateTime? DatePublished { get; set; }
+        public DateTime? DateLastModified { get; set; }
+        public DateTime DateCreated { get; set; }
+        public sbyte Draft { get; set; }
+        public string Slug { get; set; }
+    }
+}
---
+++ My_Api/Entities/Stock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace My_Api
+{
+    public partial class Stock
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public int StockLevel { get; set; }
+        public string Currency { get; set; }
+        public double UnitPrice { get; set; }
+        public string Description { get; set; }
+    }
+}
