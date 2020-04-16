cb03c4a55310a4634d31424a99cca50ccd8755c1
alexWilkinson <alex@hooq.tv>
2020-04-16 15:32:01 +0800

    commit after the database items have been intergrated in

---
+++ My_Api/AlexwilkinsonContext.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace My_Api
+{
+    public partial class AlexwilkinsonContext : DbContext
+    {
+
+        public AlexwilkinsonContext(DbContextOptions<AlexwilkinsonContext> options)
+            : base(options)
+        {
+
+        }
+
+        public virtual DbSet<MyBlog> MyBlog { get; set; }
+        public virtual DbSet<Stock> Stock { get; set; }
+        public virtual DbSet<User> User { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MyBlog>(entity =>
+            {
+                entity.ToTable("my_blog");
+
+                entity.HasIndex(e => e.Slug)
+                    .HasName("IDX_02971a36a973515c43a8d61e8a")
+                    .IsUnique();
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("id")
+                    .HasColumnType("int(11)");
+
+                entity.Property(e => e.Author)
+                    .HasColumnName("author")
+                    .HasColumnType("int(11)");
+
+                entity.Property(e => e.Body)
+                    .IsRequired()
+                    .HasColumnName("body")
+                    .HasColumnType("text")
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+
+                entity.Property(e => e.CoverImage)
+                    .IsRequired()
+                    .HasColumnName("coverImage")
+                    .HasColumnType("varchar(255)")
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+
+                entity.Property(e => e.DateCreated)
+                    .HasColumnName("dateCreated")
+                    .HasColumnType("datetime");
+
+                entity.Property(e => e.DateLastModified)
+                    .HasColumnName("dateLastModified")
+                    .HasColumnType("datetime");
+
+                entity.Property(e => e.DatePublished)
+                    .HasColumnName("datePublished")
+                    .HasColumnType("datetime");
+
+                entity.Property(e => e.Draft)
+                    .HasColumnName("draft")
+                    .HasColumnType("tinyint(4)")
+                    .HasDefaultValueSql("'1'");
+
+                entity.Property(e => e.PermaLink)
+                    .IsRequired()
+                    .HasColumnName("permaLink")
+                    .HasColumnType("varchar(255)")
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+
+                entity.Property(e => e.Slug)
+                    .IsRequired()
+                    .HasColumnName("slug")
+                    .HasColumnType("varchar(255)")
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+
+                entity.Property(e => e.Tags)
+                    .HasColumnName("tags")
+                    .HasColumnType("text")
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+
+                entity.Property(e => e.ThumbNail)
+                    .IsRequired()
+                    .HasColumnName("thumbNail")
+                    .HasColumnType("varchar(255)")
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+
+                entity.Property(e => e.TimeRequired)
+                    .HasColumnName("timeRequired")
+                    .HasColumnType("int(11)");
+
+                entity.Property(e => e.Title)
+                    .IsRequired()
+                    .HasColumnName("title")
+                    .HasColumnType("varchar(255)")
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+            });
+
+            modelBuilder.Entity<Stock>(entity =>
+            {
+                entity.ToTable("stock");
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("id")
+                    .HasColumnType("int(11)");
+
+                entity.Property(e => e.Currency)
+                    .IsRequired()
+                    .HasColumnName("currency")
+                    .HasColumnType("varchar(255)")
+                    .HasDefaultValueSql("'SGD'")
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+
+                entity.Property(e => e.Description)
+                    .HasColumnName("description")
+                    .HasColumnType("text")
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+
+                entity.Property(e => e.ImageUrl)
+                    .HasColumnName("image_url")
+                    .HasColumnType("varchar(255)")
+                    .HasDefaultValueSql("'/static/images/default_placeholder.png'")
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasColumnName("name")
+                    .HasColumnType("varchar(255)")
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+
+                entity.Property(e => e.StockLevel)
+                    .HasColumnName("stock_level")
+                    .HasColumnType("int(11)");
+
+                entity.Property(e => e.UnitPrice).HasColumnName("unit_price");
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.ToTable("user");
+
+                entity.HasIndex(e => e.Email)
+                    .HasName("IDX_e12875dfb3b1d92d7d7c5377e2")
+                    .IsUnique();
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("id")
+                    .HasColumnType("int(11)");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasColumnName("email")
+                    .HasColumnType("varchar(255)")
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasColumnName("firstName")
+                    .HasColumnType("varchar(255)")
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+
+                entity.Property(e => e.IsActive)
+                    .HasColumnName("isActive")
+                    .HasColumnType("tinyint(4)");
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasColumnName("lastName")
+                    .HasColumnType("varchar(255)")
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasColumnName("password")
+                    .HasColumnType("varchar(255)")
+                    .HasCharSet("latin1")
+                    .HasCollation("latin1_swedish_ci");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
